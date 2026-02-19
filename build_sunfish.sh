ece3996103a1ca2c49b813516e0b8cf95715e38c
Vaisakh Murali <vaisakhmurali@gmail.com>
2026-02-19 23:08:04 +0530

    build_sunfish: add a create zip step

--- build_sunfish.sh
+++ build_sunfish.sh
@@ -1,1 +1,12 @@
 BUILD_CONFIG=private/msm-google/build.config.sunfish_performance build/build.sh "$@"
+
+# Create zip
+cp out/android-msm-pixel-4.14/dist/Image.lz4-dtb ak3
+cp out/android-msm-pixel-4.14/dist/dtbo.img ak3
+cat out/android-msm-pixel-4.14/dist/*.dtb > ak3/dtb
+echo "Creating Kernel Zip"
+cd ak3
+zip -r9 eva-pixel4a-$(/bin/date -u '+%d%m%Y%I%M').zip * -x .git README.md
+mv eva-pixel4a* ../
+cd -
+echo "Created Zip! Find it in root of the working dir "
