01510efdb4151734b659adc274c8bb40092e90b4
Vaisakh Murali <vaisakhmurali@gmail.com>
2026-02-20 11:05:32 +0530

    defconfig: Build uncompressed kernel
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- build_sunfish.sh
+++ build_sunfish.sh
@@ -1,7 +1,7 @@
 BUILD_CONFIG=private/msm-google/build.config.sunfish_performance build/build.sh "$@"
 
 # Create zip
-cp out/android-msm-pixel-4.14/dist/Image.lz4-dtb ak3
+cp out/android-msm-pixel-4.14/dist/Image ak3
 cp out/android-msm-pixel-4.14/dist/dtbo.img ak3
 cat out/android-msm-pixel-4.14/dist/*.dtb > ak3/dtb
 echo "Creating Kernel Zip"
